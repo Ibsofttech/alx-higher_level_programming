10aeb34df20d5727c0336ae118a17990a9c07dbb
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-06 13:16:39 +0000

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for dgt in range(100):
-    print("{:02d}".format(dgt), end=(", " if dgt < 99 else "\n"))
-
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
