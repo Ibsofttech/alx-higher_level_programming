eedffc072772eac276043c860124b502ac4c6d3e
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-12 19:02:14 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,5 +2,6 @@
 def element_at(my_list, idx):
 	if idx < 0 or idx >= len(my_list):
 		return None
-	my_nuumber = my_list[idx]
-	return my_nuumber
+	else:
+		my_nuumber = my_list[idx]
+		return my_nuumber
