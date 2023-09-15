7830447d2278a85503f9452ed725e87491cd8a58
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-15 22:43:38 +0000

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_numerals = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
+    if roman_string is None:
+        return 0
+    result = 0
+    prev_value = 0
+
+    for i in range(len(roman_string) - 1, -1, -1):
+        value = roman_numerals.get(roman_string[i])
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+
+    return result
