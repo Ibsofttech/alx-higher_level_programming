2188ac17e7411aefad74cdc7d7eb8534f430a68a
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-15 17:41:47 +0000

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    else:
+        max = 0
+        for key, value in a_dictionary.items():
+            if value > max:
+                max = value
+        return max
