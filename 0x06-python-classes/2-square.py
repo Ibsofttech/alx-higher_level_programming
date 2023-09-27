ad48034da98acb06dd5d29f35c794e30f6ca6aab
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-27 23:13:37 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,10 @@
 
 class Square:
     def __init__(self, size=0):
+        """Attribute and methods defination"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self._size = size
+            self.__size = size
