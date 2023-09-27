25871efc9c4927a7b987c7aec3b5f3b7405a214d
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-27 23:04:39 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size > 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self._mySize = size
