e185562bb21b93fc95a4300be48108db41193476
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-09-28 00:14:05 +0000

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,13 +3,16 @@
 """Define a class Square"""
 
 
-class Square:i
+class Square:
     """Square representation"""
 
     def __init__(self, size=0):
-        if not isinstance(size, int):                     raise TypeError("size must be an integer")
-        elif size < 0:                                    raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
     @property
     def size(self):
         """Returns the new size of tne Square """
@@ -28,4 +31,3 @@ class Square:i
     def area(self):
         """Return the square."""
         return (self.__size * self.__size)
-
