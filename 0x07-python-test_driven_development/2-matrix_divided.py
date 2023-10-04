f02e4c5e95873750ae76bdbe15cc69f9a8ef8da9
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-10-04 22:02:05 +0000

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -16,21 +16,32 @@ def matrix_divided(matrix, div):
         Return:
             a new matrix whose value has been divided
     """
-    if not isinstance(div, (int, float)):
+    new_matrix = []
+    if (not isinstance(div, float) and not isinstance(div, int)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix "
+        raise TypeError("matrix must be a matrix"
                         "(list of lists) of integers/floats")
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix "
+    try:
+        size = len(matrix[0])
+    except Exception:
+        raise TypeError("matrix must be a matrix"
+                        "(list of lists) of integers/floats")
+    for i in matrix:
+        if not isinstance(i, list):
+            raise TypeError("matrix must be a matrix"
                             "(list of lists) of integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix "
-                            "must have the same size")
-        for data in row:
-            if not isinstance(data, (int, float)):
-                raise TypeError("matrix must be a matrix "
+        if len(i) != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for number in i:
+            if not isinstance(number, int) and not isinstance(number, float):
+                raise TypeError("matrix must be a matrix"
                                 "(list of lists) of integers/floats")
+            init = float(number / div)
+            ans = float("{:.2f}".format(init))
+            new_row.append(ans)
+        new_matrix.append(new_row)
+    return new_matrix
