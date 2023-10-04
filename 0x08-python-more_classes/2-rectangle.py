3a82f7298594c04f9dafdd220a8d0f23f17e3b31
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-10-04 22:30:20 +0000

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ Building a Rectangle  """
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle"""
         self.width = width
         self.height = height
 
@@ -30,3 +31,12 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
