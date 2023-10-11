1a9ba7964f1dc11c942954528cfcfd4c58e9740f
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-10-11 19:32:28 +0000

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 
 
 class MyList(list):
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
     def print_sorted(self):
-        """ Prints a list in ascending order """
+        """prints the sorted list"""
         print(sorted(self))
