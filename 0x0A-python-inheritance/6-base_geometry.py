01e5a3e59ba5838f85bb58d117a2646a079bc812
ibrahim muibi <ibrahimthesofttech@gmail.com>
2023-12-04 23:16:55 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module that creates an empty class """
+
+
+class BaseGeometry:
+    """ Defines a class BaseGeaometry """
+
+    def area(self):
+
+        raise Exception("area() is not implemented")
