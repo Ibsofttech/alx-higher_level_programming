8733a0cfdce367fbd9baa2de1bac66f811c0fc8f
ibrahim muibi <ibrahimthesofttech@gmail.com>
2024-02-13 00:44:32 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 function factorial (dgt) {
-	if ((dgt === 0) || isNaN(dgt)) {
-	  return 1;
-	} else {
-	  return dgt * factorial(dgt - 1);
-	}
+  if ((dgt === 0) || isNaN(dgt)) {
+    return 1;
+  } else {
+    return dgt * factorial(dgt - 1);
   }
-  
-  const number = parseInt(process.argv[2]);
-  console.log(factorial(number));
+}
+
+const number = parseInt(process.argv[2]);
+console.log(factorial(number));
