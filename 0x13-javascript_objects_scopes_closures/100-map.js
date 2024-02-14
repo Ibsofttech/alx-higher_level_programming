a30148e275330b563ddce9dde5419b9cabadfdcd
ibrahim muibi <ibrahimthesofttech@gmail.com>
2024-02-14 09:29:52 +0100

    task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const myList = require('./100-data.js').myList;
-console.log(myList);
-console.log(myList.map((item, index) => item * index));
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
